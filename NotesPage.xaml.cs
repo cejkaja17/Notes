db9020401e187b587993d94235cbd80fde37efac
cejkaja17 <cejkaja17@sps-prosek.cz>
2020-10-28 21:53:18 +0100

    Initial commit

---
+++ NotesPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+using Notes.Models;
+
+namespace Notes
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.Database.GetNotesAsync();
+        }
+
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+
+            Note note = new Note();
+            note.Date = DateTime.Now;
+            note.Update = DateTime.Now;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = note
+
+            });
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+    }
+}
